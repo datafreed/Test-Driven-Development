dc04da90d88e28fb969ceda2c069ecc019f2ece0
Pranay <pranayg.aiesec@gmail.com>
2018-05-04 01:11:49 +0530

    Update create-video-test.js

--- pranay-gambhir-capstone-tdd/test/routes/create-video-test.js
+++ pranay-gambhir-capstone-tdd/test/routes/create-video-test.js
@@ -5,11 +5,11 @@ const {jsdom} = require('jsdom');
 const app = require('../../app');
 const Video = require('../../models/video');
 
-const {parseTextFromHTML, buildItemObject} = require('../test-utils');
+const {parseTextFromHTML, buildVideoObject} = require('../test-utils');
 const {connectDatabaseAndDropData, diconnectDatabase} = require('../setup-teardown-utils');
 
 describe('Server path: /videos/create', () => {
-  const itemToCreate = buildItemObject();
+  const videoToCreate = buildVideoObject();
 
   beforeEach(connectDatabaseAndDropData);
 
@@ -17,9 +17,12 @@ describe('Server path: /videos/create', () => {
 
   describe('GET', () => {
     it('renders empty input fields', async () => {
+
+      // EXCERCISE
       const response = await request(app)
         .get('/videos/create');
- 
+      
+      //VERIFY
       assert.equal(parseTextFromHTML(response.text, 'input#title-input'), '');
       assert.equal(parseTextFromHTML(response.text, 'textarea#description-input'), '');
       assert.equal(parseTextFromHTML(response.text, 'input#url-input'), '');
@@ -29,61 +32,92 @@ describe('Server path: /videos/create', () => {
 
   describe('POST', () => {
     it('creates and saves a new video', async () => {
-      const itemToCreate = buildItemObject();
+
+      //SETUP
+      const videoToCreate = buildVideoObject();
+
+      //EXCERCISE
       const response = await request(app)
         .post('/videos/create')
         .type('form')
-        .send(itemToCreate);
-      const createdItem = await Video.findOne(itemToCreate);
+        .send(videoToCreate);
+      const createdItem = await Video.findOne(videoToCreate);
+
+      //VERIFY
+      assert.equal(response.status, 302);
       assert.isOk(createdItem, 'Video was not created successfully in the database');
     });
     it('redirects home', async () => {
-      const itemToCreate = buildItemObject();
+
+      //SETUP
+      const videoToCreate = buildVideoObject();
+
+      //EXCERCISE
       const response = await request(app)
         .post('/videos/create')
         .type('form')
-        .send(itemToCreate);
+        .send(videoToCreate);
+
+      //VERIFY
       assert.equal(response.status, 302);
       assert.equal(response.headers.location, '/');
     });
     it('displays an error message when supplied an empty title', async () => {
-      const invalidItemToCreate = {
+
+      //SETUP
+      const invalidVideoToCreate = {
         description: 'test',
         url: 'https://www.youtube.com/embed/a3uGWIQDbU8'
       };
+
+      //EXCERCISE
       const response = await request(app)
         .post('/videos/create')
         .type('form')
-        .send(invalidItemToCreate);
+        .send(invalidVideoToCreate);
       const allItems = await Video.find({});
+
+      //VERIFY
       assert.equal(allItems.length, 0);
       assert.equal(response.status, 400);
       assert.include(parseTextFromHTML(response.text, 'form'), 'required');
     });
     it('displays an error message when supplied an empty description', async () => {
-      const invalidItemToCreate = {
+
+      //SETUP
+      const invalidVideoToCreate = {
         title: 'test',
         url: 'https://www.youtube.com/embed/a3uGWIQDbU8'
       };
+
+      //EXCERCISE
       const response = await request(app)
         .post('/videos/create')
         .type('form')
-        .send(invalidItemToCreate);
+        .send(invalidVideoToCreate);
       const allItems = await Video.find({});
+
+      //VERIFY
       assert.equal(allItems.length, 0);
       assert.equal(response.status, 400);
       assert.include(parseTextFromHTML(response.text, 'form'), 'required');
     });
     it('displays an error message when supplied an empty url', async () => {
-      const invalidItemToCreate = {
+
+      //SETUP
+      const invalidVideoToCreate = {
         title: 'test',
         description: 'test'
       };
+
+      //EXCERCISE
       const response = await request(app)
         .post('/videos/create')
         .type('form')
-        .send(invalidItemToCreate);
+        .send(invalidVideoToCreate);
       const allItems = await Video.find({});
+
+      //VERIFY
       assert.equal(allItems.length, 0);
       assert.equal(response.status, 400);
       assert.include(parseTextFromHTML(response.text, 'form'), 'required');
