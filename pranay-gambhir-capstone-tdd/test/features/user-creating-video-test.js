f68941af623ad1a73b29da8f00588225ccdcafc4
Pranay <pranayg.aiesec@gmail.com>
2018-05-04 01:07:07 +0530

    Update user-creating-video-test.js

--- pranay-gambhir-capstone-tdd/test/features/user-creating-video-test.js
+++ pranay-gambhir-capstone-tdd/test/features/user-creating-video-test.js
@@ -1,17 +1,21 @@
 const {assert} = require('chai');
-const {buildItemObject} = require('../test-utils');
+const {buildVideoObject} = require('../test-utils');
 
 describe('User visits the create video page', () => {
     describe('posts a new video', () => {
       it('and is rendered', () => {
-        const itemToCreate = buildItemObject();
+
+        //SETUP
+        const videoToCreate = buildVideoObject();
         browser.url('/videos/create');
-        browser.setValue('#title-input', itemToCreate.title);
-        browser.setValue('#description-input', itemToCreate.description);
-        browser.setValue('#url-input', itemToCreate.url);
+        browser.setValue('#title-input', videoToCreate.title);
+        browser.setValue('#description-input', videoToCreate.description);
+        browser.setValue('#url-input', videoToCreate.url);
         browser.click('#submit-button');
-        assert.include(browser.getText('body'), itemToCreate.title);
-        assert.include(browser.getAttribute('body iframe', 'src'), itemToCreate.url);
+
+        //VERIFY
+        assert.include(browser.getText('body'), videoToCreate.title);
+        assert.include(browser.getAttribute('body iframe', 'src'), videoToCreate.url);
       });
     });
 }); 
